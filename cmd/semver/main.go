2198e44c71fd776b57c66ffe8e4058efa9fc5821
Artur Troian <troian.ap@gmail.com>
2025-01-14 21:45:55 -0500

    fix(cmd): updated version assignment (#3)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- cmd/semver/main.go
+++ cmd/semver/main.go
@@ -195,11 +195,11 @@ func bumpCmd() *cobra.Command {
 
 			switch args[0] {
 			case "major":
-				version.IncMajor()
+				*version = version.IncMajor()
 			case "minor":
-				version.IncMinor()
+				*version = version.IncMinor()
 			case "patch":
-				version.IncPatch()
+				*version = version.IncPatch()
 			case "prerel":
 				res := extractRegexp.FindAllStringSubmatch(version.Prerelease(), -1)
 				var newPrefix string
@@ -247,9 +247,8 @@ func bumpCmd() *cobra.Command {
 					return err
 				}
 
-				version = &nVer
+				*version = nVer
 			case "build":
-
 			default:
 
 			}
